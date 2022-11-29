6b7a5f58483ced66034c0f73595c82b3a2d6f0e0
lynn <lincao0224@163.com>
2022-11-29 15:52:27 +0800

    init

---
+++ index-55686bcfde.js
@@ -0,0 +1,16 @@
+/**
+ * 设置LayaNative屏幕方向，可设置以下值
+ * landscape           横屏
+ * portrait            竖屏
+ * sensor_landscape    横屏(双方向)
+ * sensor_portrait     竖屏(双方向)
+ */
+window.screenOrientation = "sensor_landscape";
+
+//-----libs-begin-----
+loadLib("libs/min/laya-78deeb5d3e.core.min.js")
+loadLib("libs/min/laya-8cae4a1ed3.ui.min.js")
+loadLib("libs/min/laya-d89c0834a1.device.min.js")
+loadLib("libs/min/laya-9f7e23d550.physics.min.js")
+//-----libs-end-------
+loadLib("js/bundle-34d061cd6e.js");
